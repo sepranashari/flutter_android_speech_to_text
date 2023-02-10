d0288b3305546c175c064b98fa0f3bed6c2a2cb7
Sepran Ashari <sepran@jogjamedia.co.id>
2023-02-11 04:31:21 +0700

    Update - Add native function

--- android/src/main/java/id/sepran/flutter_android_speech_to_text/FlutterAndroidSpeechToTextPlugin.java
+++ android/src/main/java/id/sepran/flutter_android_speech_to_text/FlutterAndroidSpeechToTextPlugin.java
@@ -1,34 +1,112 @@
 package id.sepran.flutter_android_speech_to_text;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 
+import java.util.ArrayList;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry;
 
 /** FlutterAndroidSpeechToTextPlugin */
-public class FlutterAndroidSpeechToTextPlugin implements FlutterPlugin, MethodCallHandler {
+public class FlutterAndroidSpeechToTextPlugin implements FlutterPlugin, ActivityAware, MethodCallHandler, PluginRegistry.ActivityResultListener {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
 
+  private Context context;
+
+  private Result flutterResult;
+
+  private Activity activity;
+
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
     channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_android_speech_to_text");
     channel.setMethodCallHandler(this);
+    context = flutterPluginBinding.getApplicationContext();
+  }
+
+  @Override
+  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+    activity = binding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+
   }
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+      return;
     }
+    if (call.method.equalsIgnoreCase("getSpeechToText")) {
+      flutterResult = result;
+      final String language = call.argument("language");
+      Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+      intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+      intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language);
+      //intent.putExtra(RecognizerIntent.EXTRA_PREFER_OFFLINE, true);
+      intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1);
+
+      if (intent.resolveActivity(context.getPackageManager()) != null) {
+        this.activity.startActivityForResult(intent, 2342);
+      } else {
+        Toast.makeText(context, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+      }
+      return;
+    }
+    result.notImplemented();
+  }
+
+  @Override
+  public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+    // Check the request code
+    if (requestCode == 2342) {
+      // Check the result code
+
+      if (resultCode == Activity.RESULT_OK) {
+        // Get the results
+        ArrayList<String> hasilList = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+        //flutterEventChannelResult.success(true);
+        //flutterEventChannelResult = null;
+        try{
+          flutterResult.success(hasilList.get(0));
+        }catch(Exception e){}
+        return true;
+        //sendResults();
+      } else {
+
+      }
+    }
+    return false;
   }
 
   @Override
